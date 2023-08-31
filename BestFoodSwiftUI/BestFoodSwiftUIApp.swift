234c8ee73d3835e2e3f0801797d4c0a19adde857
erenolkun <erenolkun@gmail.com>
2023-08-31 12:50:34 +0300

    Initial Commit

---
+++ BestFoodSwiftUI/BestFoodSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestFoodSwiftUIApp.swift
+//  BestFoodSwiftUI
+//
+//  Created by FERDA OLKUN on 31.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct BestFoodSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
